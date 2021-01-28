4a2beabf7cbded5541b5a3672e1e38472f697366
Vilmante <vilmantech@yahoo.com>
2021-01-28 14:55:44 +0200

    clock design, render #4 #13

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+import { clock } from './components/clock.js';
 import {  socialsData } from './data/socialData.js';
-import { renderSocials } from './components/renderSocials.js'
+import { renderSocials } from './components/renderSocials.js';
 
+
+clock('.clock');
 renderSocials('footer > .socials', socialsData);
+
